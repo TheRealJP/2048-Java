fd0abd5ca5fda94d476feb00d9bdb024bf6f18da
JoniVR <joni.vanroost@student.kdg.be>
2017-02-13 14:10:58 +0100

    Linked model with view, view updating now on actions.

--- src/views/spelview/BordView.java
+++ src/views/spelview/BordView.java
@@ -5,12 +5,14 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 
 public class BordView extends BorderPane {
-    private Label[][] labels;
+    private Label[][] labels = new Label[4][4];
     private Label currentScore;
     private Label highScore;
     private GridPane grid;
+    private boolean firstTime = true;
 
     public BordView() {
         initialiseNodes();
@@ -33,25 +35,36 @@ public class BordView extends BorderPane {
         this.setCenter(grid);
         grid.setGridLinesVisible(true);
 
-/*
-      for (int x = 0; x < labels.length; x++) {
-        for (int y = 0; y < labels[x].length; y++) {
-            grid.add(labels[x][y], x, y);
-            GridPane.setMargin(labels[x][y], new Insets(30));
-        }
-      }
-
-      //TODO: BUGFIX - momenteel doet zich hier nog een bug voor (java.lang.NullPointerException)
-*/
-
     }
 
     public GridPane getGrid() {
         return this.grid;
     }
 
-    public void setLabels(Label[][] labels) {
-        this.labels = labels;
+    public void setLabels(int[][] waardes) {
+
+        if (firstTime) {
+
+            for (int x = 0; x < labels.length; x++) {
+                for (int y = 0; y < labels[x].length; y++) {
+
+                    labels[x][y] = new Label("" + waardes[y][x]);
+                    grid.add(labels[x][y], x, y);
+                    GridPane.setMargin(labels[x][y], new Insets(50));
+                    labels[x][y].setFont(new Font(50)); // fontsize instellen (wss tijdelijk tot css)
+                    firstTime = false;
+                }
+            }
+        } else {
+
+            for (int x = 0; x <labels.length; x++) {
+                for (int y = 0; y < labels[x].length; y++) {
+
+                    labels[x][y].textProperty().setValue(""+waardes[y][x]);
+                }
+            }
+
+        }
     }
 
     public Label getHighScore() {
