db817a3498b7fd6fe7a86c7ca760f10d599f252d
JoniVR <joni.vanroost@student.kdg.be>
2017-02-28 17:34:13 +0100

    Improved TopScoreView code for easier visual setup.

--- src/views/topscoreview/TopScoreView.java
+++ src/views/topscoreview/TopScoreView.java
@@ -16,13 +16,15 @@ public class TopScoreView extends BorderPane {
     private Label schermNaamLbl; //het grote, centrale label
     private Label naamLbl; // het vaste label "naam"
     private Label scoreLbl; // het vaste label "score"
-    private HBox naamEnScoreBox; //
-    private VBox schermLabels; // alle vaste labels van het scherm worden hierin gestoken
-    private Font fontSize = new Font(50); //TODO: VERVANGEN IN CSS!!!
+    private Font fontSize = new Font(40); //TODO: VERVANGEN IN CSS!!!
     private Label[] lblSpelernamen;
     private Label[] lblSpelerScores;
-    private HBox[] hBoxSpeler;
-    private VBox scoreInhoudVBox;
+
+    private HBox naamEnScoreBox; // box met het label Naam & Score (vaste labels)
+    private VBox spelerNaamInhoudVBox; //VBOX voor de spelernamen
+    private VBox scoreInhoudVBox; //VBOX voor de spelerscores
+    private HBox inhoudBox; // HBox met de naam en score VBOXEN
+    private VBox schermVBox; // VBOX met: NaamEnScoreBox (HBOX) en de inhoudbox (HBOX)
 
     public TopScoreView() {
 
@@ -32,11 +34,17 @@ public class TopScoreView extends BorderPane {
 
     private void initialiseNodes() {
 
+        // setup labels
         schermNaamLbl = new Label("Top scores:");
         naamLbl = new Label("Naam");
         scoreLbl = new Label("Score");
+
+        // setup Boxen
         naamEnScoreBox = new HBox(300, naamLbl, scoreLbl);
-        schermLabels = new VBox(20, schermNaamLbl, naamEnScoreBox);
+        spelerNaamInhoudVBox = new VBox();
+        scoreInhoudVBox = new VBox();
+        inhoudBox = new HBox(300);
+        schermVBox = new VBox(20,naamEnScoreBox,inhoudBox);
     }
 
     private void layoutNodes() {
@@ -44,16 +52,16 @@ public class TopScoreView extends BorderPane {
         // labels
         schermNaamLbl.setPadding(new Insets(30, 0, 0, 0));
 
-        //TODO: DEZE VERVANGEN DOOR CSS!!!
+        // Fontsize - TODO: DEZE VERVANGEN DOOR CSS!!!
         schermNaamLbl.setFont(fontSize);
         naamLbl.setFont(fontSize);
         scoreLbl.setFont(fontSize);
 
-        naamEnScoreBox.setAlignment(Pos.CENTER);
-        schermLabels.setAlignment(Pos.CENTER);
-
-        setTop(schermLabels);
-        setAlignment(schermLabels, Pos.BOTTOM_CENTER);
+        // positionering boxen
+        setTop(schermNaamLbl);
+        setAlignment(schermNaamLbl,Pos.CENTER);
+        setCenter(schermVBox);
+        setAlignment(schermVBox,Pos.CENTER);
     }
 
     public void setSpelers(ArrayList<Speler> spelerLijst) {
@@ -61,22 +69,21 @@ public class TopScoreView extends BorderPane {
         // de reden dat we hier altijd met de size van de list werken is omdat er niet altijd 10 topscores zijn.
         final int aantal = spelerLijst.size();
 
+        // grootte van de arrays instellen op de lengte van de lijst TODO: Dit moet nog getest worden met minder dan 10 spelers
         lblSpelernamen = new Label[aantal];
         lblSpelerScores = new Label[aantal];
-        hBoxSpeler = new HBox[aantal];
 
+        // Labels opvullen voor elke speler
         for (int i = 0; i < aantal; i++) {
-            lblSpelernamen[i] = new Label((i + 1)+". "+spelerLijst.get(i).getNaam());
-            lblSpelernamen[i].setFont(fontSize);
-            lblSpelerScores[i] = new Label("" + spelerLijst.get(i).getScore());
-            lblSpelerScores[i].setFont(fontSize);
-            hBoxSpeler[i] = new HBox(300,lblSpelernamen[i], lblSpelerScores[i]);
-            hBoxSpeler[i].setAlignment(Pos.CENTER);
+
+            lblSpelernamen[i] = new Label(i+1 +".  "+spelerLijst.get(i).getNaam());
+            lblSpelerScores[i] = new Label(""+spelerLijst.get(i).getScore());
+
+            // labels toevoegen aan de correcte boxen
+            spelerNaamInhoudVBox.getChildren().add(lblSpelernamen[i]);
+            scoreInhoudVBox.getChildren().add(lblSpelerScores[i]);
         }
-        scoreInhoudVBox = new VBox(hBoxSpeler);
-        scoreInhoudVBox.setAlignment(Pos.TOP_CENTER);
-        scoreInhoudVBox.setPadding(new Insets(40,0,0,0));
-        setCenter(scoreInhoudVBox);
-        setAlignment(scoreInhoudVBox, Pos.CENTER);
+        // de boxen toevoegen aan de inhoudbox
+        inhoudBox.getChildren().addAll(spelerNaamInhoudVBox,scoreInhoudVBox);
     }
 }
